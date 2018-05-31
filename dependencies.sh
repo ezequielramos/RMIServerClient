b7de27ea8e566cc97f284d157777254eac81269f
Ezequiel Ramos <ezequielmr94@gmail.com>
2018-05-31 20:42:22 -0300

    created requirements file and install dependencies

---
+++ dependencies.sh
@@ -0,0 +1,4 @@
+ #!/bin/bash
+apt-get install python-pip python-dev
+
+python -m pip install -r requirements.txt
--- greeting-server.py
+++ greeting-server.py
@@ -4,16 +4,19 @@ import Pyro4
 class GreetingMaker(object):
     def get_fortune(self, name):
         print('Someone called get_fortune method')
-	return nameServer + ": Hello, {0}. Here is your fortune message:\n" \
-               "Tomorrow's lucky number is 12345678.".format(name)
+        return nameServer + ": Hello, {0}. Here is your fortune message:\n Tomorrow's lucky number is 12345678.".format(name)
+
+daemon = Pyro4.Daemon()                # make a Pyro daemon
+try:
+    ns = Pyro4.locateNS()                  # find the name server
+except Pyro4.errors.NamingError:
+    print('You need to run pyro4-ns before you start a server.')
+    exit()
 
 nameServer = raw_input('Insert server name: ')
 
-daemon = Pyro4.Daemon()                # make a Pyro daemon
-ns = Pyro4.locateNS()                  # find the name server
 uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
 ns.register('greeting-' + nameServer, uri)   # register the object with a name in the name server
 
 print("Ready.")
 daemon.requestLoop()                   # start the event loop of the server to wait for calls
-
