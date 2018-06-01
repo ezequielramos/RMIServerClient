7f5419879c8cf5e58e0e3adbf34224d456f07838
Ezequiel Ramos <ezequielmr94@gmail.com>
2018-06-01 01:09:22 -0300

    created replica services where main server send the replica to the other ones

--- rmiechoserver.py
+++ rmiechoserver.py
@@ -7,12 +7,24 @@ class RMIEchoServer(object):
         self.aMessages = list()
         self.name_server = name_server
 
-    def messageStorage(self, message):
+    def sendMessageToReplicas(self, name_server, message):    
+        ns = Pyro4.locateNS()
+        server_names = ns.list('greeting-') #this should be returning me all servers registered on pyro's nameserver
+        keys = list(server_names.keys())
+
+        for key in keys:
+            each_server = Pyro4.Proxy(server_names[key])
+            try:
+                each_server.receiveMessageToReplica(name_server, message)
+            except Pyro4.errors.CommunicationError:
+                print('Can\'t send message to server ' + key.split('greeting-')[1])
+
+    def receiveMessageToReplica(self, name_server, message):
+        print('Received message from server ' + name_server)
         self.aMessages.append(message)
-        print(self.aMessages)
 
     def echoService(self, message):
-        self.messageStorage(message)
+        self.sendMessageToReplicas(self.name_server, str(message))
         return self.name_server + ": " + str(message)
 
     def showMessages(self):
