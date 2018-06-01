3d4add90d420c60084abb9f26af5821de794b583
Ezequiel Ramos <ezequielmr94@gmail.com>
2018-05-31 21:02:32 -0300

    changed error mesage

--- greeting-server.py
+++ greeting-server.py
@@ -45,7 +45,7 @@ daemon = Pyro4.Daemon()                # make a Pyro daemon
 try:
     ns = Pyro4.locateNS()                  # find the name server
 except Pyro4.errors.NamingError:
-    print('You need to run pyro4-ns before you start a server.')
+    print("\nFailed to locate the nameserver. Make sure it's running, execute: \n\npyro4-ns\n")
     exit()
 
 nameServer = raw_input('Insert server name: ')
